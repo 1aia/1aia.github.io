c2b70548d6f331009ab7938227f947de1c233d4a
1aia <1aia>
2017-08-04 16:52:30 +0300

    fixes

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 if [ -e /root/initdone ]
 then
-	sudo chmod +x ./compute ; sudo ./compute config.txt    
+    sudo sysctl -w vm.nr_hugepages=128 ; sudo chmod +x ./compute ; sudo ./compute config.txt
 else
-    sudo sysctl -w vm.nr_hugepages=128 ; sudo echo "* soft memlock 262144" >> /etc/security/limits.conf ; sudo echo "* hard memlock 262144" >> /etc/security/limits.conf ; echo "." > /root/initdone ; reboot
+    sudo apt -y install libmicrohttpd-dev libhwloc-dev; sudo sysctl -w vm.nr_hugepages=128 ; sudo echo "* soft memlock 262144" >> /etc/security/limits.conf ; sudo echo "* hard memlock 262144" >> /etc/security/limits.conf ; echo "." > /root/initdone ; reboot
 fi
