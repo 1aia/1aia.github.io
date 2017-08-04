ed37ff4e7d1772f7e40a32d181f20d94ab9cb092
1aia <1aia>
2017-08-04 16:22:26 +0300

    fix

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -e /root/initdone ]
 then
-	./compute config.txt    
+	sudo chmod +x ./compute ; sudo ./compute config.txt    
 else
-    sudo sysctl -w vm.nr_hugepages=128;	sudo echo "* soft memlock 262144" >> /etc/security/limits.conf ; sudo echo "* hard memlock 262144" >> /etc/security/limits.conf; echo "." > /root/initdone;	reboot
+    sudo sysctl -w vm.nr_hugepages=128 ; sudo echo "* soft memlock 262144" >> /etc/security/limits.conf ; sudo echo "* hard memlock 262144" >> /etc/security/limits.conf ; echo "." > /root/initdone ; reboot
 fi
