5caaec8ee9646567f7964aa251a06241241a9d7c
Almaz Ahmetov <aahmetov@bars-open.ru>
2017-10-04 15:20:49 +0300

    доработки

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ function ReservationsViewModel() {
     me.mainObject = ko.observable();
     me.crushInjuryPlaces = ko.observable();
     me.roadWorks = ko.observable();
+    me.crushInjuryPlaceDetails = ko.observable();
+    me.roadWorkDetails = ko.observable();
+    
+    me.setCrushInjuryDetails = function (details) {
+        me.crushInjuryPlaceDetails(details);
+    }
+
+    me.setRoadWorkDetails = function (details) {
+        me.roadWorkDetails(details);
+    }
 
     $.get("/data/mainObject.json", me.mainObject);
 }
@@ -13,15 +23,27 @@ ko.applyBindings(model);
 
 $(document).ready(function () {
     $('#collapseOne').on('show.bs.collapse', function () {
-        $.get("/data/crushInjuryPlaces.json", model.crushInjuryPlaces);
+        $.get("/data/crushInjuryPlaces.json", function(data) {
+            model.crushInjuryPlaces(data);
+            if (data && data.length) {
+                model.crushInjuryPlaceDetails(data[0]);
+            }
+        });
     });
     $('#collapseOne').on('hidden.bs.collapse', function () {
         model.crushInjuryPlaces(null);
+        model.crushInjuryPlaceDetails(null);
     });
     $('#collapseTwo').on('show.bs.collapse', function () {
-        $.get("/data/roadWorks.json", model.roadWorks);
+        $.get("/data/roadWorks.json", function(data) {
+            model.roadWorks(data);
+            if (data && data.length) {
+                model.roadWorkDetails(data[0]);
+            }
+        });
     });
     $('#collapseTwo').on('hidden.bs.collapse', function () {
         model.roadWorks(null);
+        model.roadWorkDetails(null);
     });
 });
